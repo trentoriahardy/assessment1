902daa0985ebc7926e23eb7b4fb340f82f84bc14
COR Students <corstudents@Matts-MBP-68.localdomain>
2018-10-22 12:12:31 -0400

    finished assessment

---
+++ assessment.js
@@ -0,0 +1,76 @@
+//Question 1: Create a variable of each of the following data types 
+//String, Int, Bool, Float
+
+//let name = "Trentoria";
+//let jerseyNumber = 11;
+//let trueOrFalse = false;
+//let pi = 7.27;
+
+//Question 2: Create a variable called trueOrFalse. Create an if 
+//statement so that if the variable is true the number 1 is
+//returned. Otherwise the number 2 is returned.
+
+let trueOrFalse = numbers(true)
+let trueOrFalse = number(false)
+
+if (trueOrFalse == true){
+        if(numbers===true) {
+                return 1
+            } else {
+                return 
+        }
+
+
+//Question 3: Create a loop that counts every EVEN number from 
+//1-100 starting at 0
+
+for (i=0; i<=100; i++) {
+        if (i === 0) {
+                console.log(i +  " is even");
+        }
+        else if (i % 2 === 0) {
+                console.log(i + " is even");   
+        }
+        
+}
+
+
+//Question 4: Create a variable that is a bool and is equal to true
+//Create a while loop that will print "hello"
+//to the console 20 times and then change that variable 
+//to false ending the loop.
+
+//let greeting = true;
+//let counter = 20
+
+//while(greeting == true){
+  //      console.log("Hello");
+  
+
+
+
+//Question 5: Create a function that can determine if a parameter
+//is true or false. If it is true return the string "true.
+//Otherwise, return the string "false"
+
+//let pink = pinkOrPurple(true);
+//let purple = pinkOrPurple(false);
+
+//function pinkOrPurple(color) {
+  //  if(color===true) {
+    //    return "pink"
+   // } else {
+     //   return "purple"
+//}
+
+//}
+//if(pink==="pink") {
+  //  console.log("true")
+//}
+//if(purple==="purple"){
+  //  console.log("false")
+//}
+
+
+//Bonus Question: What does D.R.Y stand for?
+//Don't Repeat Yourself
